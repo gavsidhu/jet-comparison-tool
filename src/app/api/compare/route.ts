49c0f4a0eb11e50575e0a4bf105976c1fecbb20b
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2024-03-13 13:46:28 -0700

    chore: added comments to document code

--- src/app/api/compare/route.ts
+++ src/app/api/compare/route.ts
@@ -1,6 +1,8 @@
 import openai from "@/lib/openai"
 import { Jet } from "@prisma/client"
 
+
+// Define what the result from OpenAI should look like
 const exampleJSON = {
     name: "<string: name of the jet>", value: "<string/integer: value of the jet for the comparison>"
 }
@@ -25,6 +27,7 @@ export async function POST(request: Request) {
 
     let promptMsg = `Compare and rank the following jets by ${criteriaPrompts[criteria]}:\n`;
 
+    // Dynamically add jets to the prompt
     jets.forEach(jet => {
         promptMsg += `${jet.name}\n`
     })
@@ -32,6 +35,7 @@ export async function POST(request: Request) {
     const completion = await openai.chat.completions.create({
         messages: [
             {
+                // The word JSON needs to be included in the content. From OpenAI API docs
                 role: "system",
                 content: `You are a helpful assistant that compares jets by a comparison critera and outputs the results as a JSON Array. Each object should have the following structure: ${JSON.stringify(exampleJSON)}. Return the data under the key "results". The objects should be `,
             },
@@ -45,6 +49,7 @@ export async function POST(request: Request) {
 
     const res = JSON.parse(completion.choices[0].message.content?.trim() as string)
 
+    // Make sure the data is sorted by value before sending to client. Client will then use index to as rank column value
     const sortedResults = res.results.sort((a: any, b: any) => parseInt(b.value) - parseInt(a.value));
     console.log(sortedResults)
 
